5292e0e51ab3b57536f1c022ec6cc86bf0ad8040
Veronika <veronika-mg@bk.ru>
2023-04-19 22:56:25 +0300

    add first_letter

--- str_func.py
+++ str_func.py
@@ -1,2 +1,16 @@
 def str_upper(str):
+    """
+    Переводит все буквы в верхний регистр.
+    :param str:
+    :return:
+    """
     return str.upper()
+
+
+def first_letter(str):
+    """
+    Переводит первые буквы в заглавные.
+    :param str: str
+    :return: строку с заглавными первыми буквами.
+    """
+    return ' '.join(map(lambda x: x.capitalize(), str.split(' ')))
